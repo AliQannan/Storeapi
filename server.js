cbbdad60f05402d62378564fc44b8adc41770440
EC2 Default User <ec2-user@ip-172-31-42-31.eu-north-1.compute.internal>
2025-08-26 15:57:20 +0000

    models

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
@@ -27,7 +32,7 @@ app.use(
 app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
-  return res.json("running good");
+  res.sendFile(path.join(__dirname, "admin", "login", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
