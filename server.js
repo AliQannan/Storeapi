aaeed6809079a9794dbfd05f19378fc14d060ca6
EC2 Default User <ec2-user@ip-172-31-42-31.eu-north-1.compute.internal>
2025-08-26 09:03:53 +0000

    real

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+app.get("/" , (req,res)=>{
+
+	return res.json(" running good")
+
+	
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
