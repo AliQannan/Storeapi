30cf2e69958911c23c6b2332042e558c195fc8a0
AliQannan <126164190+AliQannan@users.noreply.github.com>
2025-08-27 09:00:49 +0300

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,7 +5,7 @@ import { loginAdmin, getAdmins } from "../controllers/adminController.js";
 
 const router = express.Router();
 
-router.post("/", loginAdmin);        // Login
+router.post("/login", loginAdmin);        // Login
 router.get("/all", getAdmins);       // Get all admins
 
 
