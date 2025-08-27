e5b30f486cfc474482de08226e37d7474f142355
AliQannan <126164190+AliQannan@users.noreply.github.com>
2025-08-27 17:16:26 +0300

    Update authAdmin.js

--- middleware/authAdmin.js
+++ middleware/authAdmin.js
@@ -1,4 +1,4 @@
-
+// Updated authAdmin middleware
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
@@ -20,8 +20,8 @@ const authAdmin = async (req, res, next) => {
     // Verify token using environment variable
     const atoken_decode = jwt.verify(atoken, process.env.JWT_ADMIN_SECRET || process.env.JWT_SECRET);
     
-    // Add admin ID to request body
-    req.body.adminId = atoken_decode.id;
+    // Add admin ID to request object instead of body
+    req.adminId = atoken_decode.id;
 
     next();
   } catch (err) {
